24a6c4a632f52aa19028b454d54cdb5827718f21
IgnacioSainz <isainzmaza.3@gmail.com>
2023-01-26 12:21:50 +0100

    Repositorio Maven

---
+++ SuperMerCasa/src/main/java/com/example/supermercasa/SuperMerCasaApplication.java
@@ -0,0 +1,13 @@
+package com.example.supermercasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperMerCasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperMerCasaApplication.class, args);
+	}
+
+}
--- SuperMerCasa/src/test/java/com/example/supermercasa/SuperMerCasaApplicationTests.java
+++ SuperMerCasa/src/test/java/com/example/supermercasa/SuperMerCasaApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.supermercasa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Demo1ApplicationTests {
+class SuperMerCasaApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
