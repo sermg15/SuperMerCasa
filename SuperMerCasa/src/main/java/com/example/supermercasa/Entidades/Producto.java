681486c9d39bf7365b189e4fdcc8060d8736d060
Mario García Barrero <m.garciaba.2019@alumnos.urjc.es>
2023-02-07 01:06:10 +0100

    Configurada la base de datos y creada una entidad de prueba

---
+++ SuperMerCasa/src/main/java/com/example/supermercasa/Entidades/Producto.java
@@ -0,0 +1,52 @@
+package com.example.supermercasa.Entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private int stock;
+
+    public Producto(long id, String name, int stock){
+        this.id = id;
+        this.name = name;
+        this.stock = stock;
+    }
+
+    public Producto() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
+
