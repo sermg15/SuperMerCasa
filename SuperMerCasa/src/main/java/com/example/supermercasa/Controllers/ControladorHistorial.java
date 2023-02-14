ee804f4c362a4bb7326993ece2101271b6c0c0c0
IgnacioSainz <isainzmaza.3@gmail.com>
2023-02-14 13:57:22 +0100

    HistorialPedidos Final

--- SuperMerCasa/src/main/java/com/example/supermercasa/Controllers/ControladorHistorial.java
+++ SuperMerCasa/src/main/java/com/example/supermercasa/Controllers/ControladorHistorial.java
@@ -10,6 +10,27 @@ public class ControladorHistorial {
     @GetMapping("/historial")
     public String historialPedidos(Model model){
         model.addAttribute("name", "Pedro");
+
+
+        model.addAttribute("pedido1", "(NumPedido)");
+        model.addAttribute("pedido2", "(NumPedido)");
+        model.addAttribute("pedido3", "(NumPedido)");
+        model.addAttribute("pedido4", "(NumPedido)");
+
+        model.addAttribute("precio1", "(precio)");
+        model.addAttribute("precio2", "(precio)");
+        model.addAttribute("precio3", "(precio)");
+        model.addAttribute("precio4", "(precio)");
+
+        model.addAttribute("fecha1", "(fecha)");
+        model.addAttribute("fecha2", "(fecha)");
+        model.addAttribute("fecha3", "(fecha)");
+        model.addAttribute("fecha4", "(fecha)");
+
+        model.addAttribute("listadoproductos1", "(lista)");
+        model.addAttribute("listadoproductos2", "(lista)");
+        model.addAttribute("listadoproductos3", "(lista)");
+        model.addAttribute("listadoproductos4", "(lista)");
         return "historialPedidos";
     }
 }
