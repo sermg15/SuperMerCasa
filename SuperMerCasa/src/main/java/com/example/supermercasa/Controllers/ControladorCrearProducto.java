89083db16eccfd4e8f17f440a4398bc0ab2cab77
sermg15 <s.martinezg.2001@gmail.com>
2023-03-01 01:16:15 +0100

    pagina para añadir productos a la base de datos
    Falta meter las fotos de cada producto, pero funciona bien. Se puede retocar algún aspecto

---
+++ SuperMerCasa/src/main/java/com/example/supermercasa/Controllers/ControladorCrearProducto.java
@@ -0,0 +1,38 @@
+package com.example.supermercasa.Controllers;
+
+import com.example.supermercasa.Entidades.Producto;
+import com.example.supermercasa.Repositorios.RepositorioOferta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ControladorCrearProducto {
+
+    @Autowired
+    public RepositorioOferta repositorioOferta;
+
+    int id = 0;
+    @GetMapping("/nuevoProducto")
+    public String nuevoProducto(Model model, @RequestParam String nameProd, @RequestParam String description,
+                                @RequestParam String price, @RequestParam int nstock){
+        if(repositorioOferta.getProductoByName(nameProd) == null){
+            //el producto no existe, por lo que se puede crear
+            repositorioOferta.save(new Producto(id, nameProd, nstock, description, Double.parseDouble(price), null));
+            model.addAttribute("mensajeAddProd", "Producto añadido con éxito");
+        }else{
+            //el producto existe, por lo que no se puede crear
+            model.addAttribute("mensajeAddProd", "Producto ya existente en la base de datos");
+        }
+        return "administrarProductos";
+    }
+
+    @GetMapping("/administrarProductos")
+    public String administrarProductos(Model model){
+        model.addAttribute("mensajeAddProd", "");
+
+        return "administrarProductos";
+    }
+}
--- SuperMerCasa/src/main/java/com/example/supermercasa/Repositorios/RepositorioOferta.java
+++ SuperMerCasa/src/main/java/com/example/supermercasa/Repositorios/RepositorioOferta.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RepositorioOferta extends JpaRepository<Producto, Long> {
 
+    Producto getProductoByName(String nombre);
 }
