86a866fef1778166ccca9f1180b3d595b19cf098
Mario García Barrero <m.garciaba.2019@alumnos.urjc.es>
2023-04-25 18:24:17 +0200

    HazelCastHttpSession

--- SuperMerCasa/src/main/java/com/example/supermercasa/SuperMerCasaApplication.java
+++ SuperMerCasa/src/main/java/com/example/supermercasa/SuperMerCasaApplication.java
@@ -2,13 +2,32 @@ package com.example.supermercasa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+import java.util.Collections;
+import org.springframework.context.annotation.Bean;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
 
 @SpringBootApplication
+@EnableHazelcastHttpSession
 public class
 SuperMerCasaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperMerCasaApplication.class, args);
+	}
+
+	@Bean
+	public Config config() {
+
+		Config config = new Config();
+
+		JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+
+		joinConfig.getMulticastConfig().setEnabled(false);
+		joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Collections.singletonList("192.168.20.90"));
+
+		return config;
 	}
 
 }
